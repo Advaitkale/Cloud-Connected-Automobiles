920dcfed6b6d0fcb12775af349b872ee079b35bb
Advait Kale <34103551+Advaitkale@users.noreply.github.com>
2019-09-17 15:09:24 +0530

    Two computer connection done

--- Client.py
+++ Client.py
@@ -1,17 +1,35 @@
-import socket
-import time
-# Create a socket object
+
+import time, socket, sys
+
+print("\nWelcome to Chat Room\n")
+print("Initialising....\n")
+time.sleep(1)
+
 s = socket.socket()
+shost = socket.gethostname()
+ip = socket.gethostbyname(shost)
+print(shost, "(", ip, ")\n")
+host = input(str("Enter server address: "))
+name = input(str("\nEnter your name: "))
+port = 1234
+print("\nTrying to connect to ", host, "(", port, ")\n")
+time.sleep(1)
+s.connect((host, port))
+print("Connected...\n")
 
-# Define the port on which you want to connect
-port = 12345
+s.send(name.encode())
+s_name = s.recv(1024)
+s_name = s_name.decode()
+print(s_name, "has joined the chat room\nEnter [e] to exit chat room\n")
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
-
-# receive data from the server
-print s.recv(1024)
-s.send("Advait")
-
-# close the connection
-s.close()
+while True:
+    message = s.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
+    message = input(str("Me : "))
+    if message == "[e]":
+        message = "Left chat room!"
+        s.send(message.encode())
+        print("\n")
+        break
+    s.send(message.encode())
